2de0e2199785816c5babe2321245be6fb7337d60
ChilsonJ <joe00529@gmail.com>
2021-05-16 04:23:06 +0800

    arduino serial readString()

--- src/arduinoPart/marsh.ino
+++ src/arduinoPart/marsh.ino
@@ -5,16 +5,14 @@ TinyStepper_28BYJ_48 stepper;
 
 unsigned long timeStamp, recentTime;
 
-const byte numChars = 32;
-char receivedChars[numChars];
-
 int runSpeed = 0, runTime = 0;
-boolean newData = false;
 
 void setup() {
   Serial.begin(9600);
   stepper.connectToPins(8, 9, 10, 11);
   stepper.setAccelerationInStepsPerSecondPerSecond(512);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
@@ -22,13 +20,15 @@ void loop() {
   timeStamp = millis();
 
   recvInfo(); // get speed
+  delay(20);
   recvInfo(); // get time
-  showNewData();
+  //showNewData();
 
   if (runSpeed != 0 && runTime != 0) {
     recentTime = millis();
     while (recentTime < timeStamp + runTime) {
       stepper.moveRelativeInSteps(2048); // half round
+      recentTime = millis();
     }
   }
 
@@ -36,46 +36,45 @@ void loop() {
   runTime = 0;
 
   delay(2000);
-
 }
-
+/*
 void recvInfo() {
   static byte ndx = 0;
   char endMarker = '\n';
-  char rc;
+  String rc;
 
-  while (Serial.available() > 0 && newData == false) {
-    rc = Serial.read();
-
-    if (rc != endMarker) {
-      receivedChars[ndx] = rc;
-      ndx++;
-      if (ndx >= numChars) {
-        ndx = numChars - 1;
-      }
-    }
-    else {
-      receivedChars[ndx] = '\0'; // terminate the string
-      ndx = 0;
-      newData = true;
-    }
+  for (int i = 0; i < 2; ++i) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
   }
 
-  if (runSpeed == 0) {
-    runSpeed = atoi(receivedChars);
-    stepper.setSpeedInStepsPerSecond(runSpeed * 10);
-  }
-  else {
-    runTime = atoi(receivedChars);
+  while (Serial.available() > 0) {
+    rc = Serial.readString();
+    runSpeed = rc.toInt();
+    //stepper.setSpeedInStepsPerSecond(runSpeed * 10);
+
+    //rc = Serial.read() - '0';
+    //runTime = rc * 1000;
+
+    Serial.println(runSpeed);
   }
 }
+*/
 
-void showNewData() {
-  if (newData == true) {
-    Serial.println("The runSpeed is:");
-    Serial.println(runSpeed);
-    Serial.println("The runTime is:");
-    Serial.println(runTime);
-    newData = false;
+void recvInfo() {
+  String rc;
+
+  while (Serial.available() > 0) {
+    rc = Serial.readString();
+
+    if (runSpeed == 0) {
+      runSpeed = rc.toInt();
+      stepper.setSpeedInStepsPerSecond(runSpeed * 10);
+    }
+    else {
+      runTime = rc.toInt() * 1000;
+    }
   }
 }
